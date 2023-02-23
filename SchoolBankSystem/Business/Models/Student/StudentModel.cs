8a01d0b7d680dea1f5701bbe1fd460f5083b3097
AFL\dzmitry.karkanitsa <aakarkanica@gmail.com>
2023-02-23 18:37:38 +0300

    develop - edit StudentModel.

--- SchoolBankSystem/Business/Models/Student/StudentModel.cs
+++ SchoolBankSystem/Business/Models/Student/StudentModel.cs
@@ -1,4 +1,8 @@
-﻿namespace Business.Models.Student
+﻿using Business.Models.Certificate;
+using Business.Models.MoneyTransfer;
+using Business.Models.StudentReward;
+
+namespace Business.Models.Student
 {
     public class StudentModel
     {
@@ -11,5 +15,13 @@
         public string Phone { get; set; }
 
         public decimal Sum { get; set; }
+
+        public List<CertificateModel> CertificatePurchases { get; set; }
+
+        public List<MoneyTransferModel> MoneyTransfersFromStudent { get; set; }
+
+        public List<MoneyTransferModel> MoneyTransfersToStudent { get; set; }
+
+        public List<StudentRewardModel> StudentRewards { get; set; }
     }
 }
